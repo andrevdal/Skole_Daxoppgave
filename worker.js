a561285287a3c8dcca85fb6295db803388778232
andrevdal <andreas.drevdal@gmail.com>
2022-01-25 15:26:53 +0100

    bug fix

--- worker.js
+++ worker.js
@@ -1,10 +1,19 @@
 function randomize() {
     return Math.floor(Math.random() * 1000)
 }
-while(true){
-    let seconds = new Date().getSeconds()
-    if(seconds === 59){
-        postMessage(randomize());
-        break
+function wait(time) {
+    return new Promise((res) => {
+        setTimeout(res, time);
+    })
+}
+
+async function timer() {
+    while(true){
+        let seconds = new Date().getSeconds()
+        if(seconds === 59){
+            await wait(1000);
+            postMessage(randomize());
+        }
     }
 }
+timer();
